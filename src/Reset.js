cb290d70f0a0b87634bd5750c3b26820a78772cb
administrator <administrator@iMAC-8.local>
2018-04-04 10:02:57 +0300

    +reset

--- src/Reset.js
+++ src/Reset.js
@@ -15,7 +15,7 @@ export default class Reset extends Component {
         )
 
         const users = [
-            { _id: 'Joe', likes: [] },
+            { _id: 'Joe', password: "", likes: [], contacts: [], feedbacks: [], messages: [], },
             { _id: 'Fred', likes: [] },
             { _id: 'Ann', likes: [] }
         ]
@@ -26,7 +26,7 @@ export default class Reset extends Component {
         )
 
         const items = [
-            { description: 'Doll', seller: 'Joe', buyer: '', expiry: '2018-02-28', highbid: '', bids: [] },
+            { name: "", imageUrl: "", category:"", description: 'Doll', seller: 'Joe', buyer: '', expiry: '2018-02-28', highbid: '', bids: [] },
             { description: 'Toy truck', seller: 'Joe', buyer: '', expiry: '2018-03-29', highbid: 10, bids: [{ username: 'Fred', amount: 10 }] },
             { description: 'Skateboard', seller: 'Ann', buyer: '', expiry: '2018-04-13', highbid: 20, bids: [{ username: 'Fred', amount: 10 }, { username: 'Ann', amount: 20 }] },
         ]
