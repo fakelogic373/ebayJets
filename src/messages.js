28b0b4b18eb5ac18664df7dd68d51dace07ae234
administrator <administrator@iMAC-8.local>
2018-04-04 14:58:39 +0300

    +messges working to correct contacts

--- src/messages.js
+++ src/messages.js
@@ -19,96 +19,96 @@ import Select from 'material-ui/Select';
 
 
 
-export class all extends Component {
+// export class all extends Component {
 
-    state = {
-        search: '',
-        select: null,
-        _id: '',
-        query: 'users'
-    }
+//     state = {
+//         search: '',
+//         select: null,
+//         _id: '',
+//         query: 'users'
+//     }
 
-    formatListUser(user, i) {
-        return (
-            <ListItem key={i}>
+//     formatListUser(user, i) {
+//         return (
+//             <ListItem key={i}>
 
-                {user.username}
+//                 {user.username}
 
-                <ListItemSecondaryAction>
-                    {
-                        db.user
-                        &&
-                        <Button variant="raised" color="primary" size="small" onClick={() => this.handleDelete(user)}>Delete</Button>
-                    }
-                    {
-                        db.user
-                        &&
-                        <Button variant="raised" color="primary" size="small" onClick={() => this.handleSelect(user)}>message</Button>
-                    }
+//                 <ListItemSecondaryAction>
+//                     {
+//                         db.user
+//                         &&
+//                         <Button variant="raised" color="primary" size="small" onClick={() => this.handleDelete(user)}>Delete</Button>
+//                     }
+//                     {
+//                         db.user
+//                         &&
+//                         <Button variant="raised" color="primary" size="small" onClick={() => this.handleSelect(user)}>message</Button>
+//                     }
 
-                    <Button variant="raised" color="primary" size="small" component={Link} to={`/user/${user._id}`}>View profile page</Button>
-                </ListItemSecondaryAction>
+//                     <Button variant="raised" color="primary" size="small" component={Link} to={`/user/${user._id}`}>View profile page</Button>
+//                 </ListItemSecondaryAction>
 
-            </ListItem>
-        )
-    }
+//             </ListItem>
+//         )
+//     }
 
-    handleSearchUsers() {
-        this.setState({ _id: '', query: 'users/username/' + this.state.search })
-    }
+//     handleSearchUsers() {
+//         this.setState({ _id: '', query: 'users/username/' + this.state.search })
+//     }
 
-    async handleDelete(user) {
-        await db.collection('users').deleteOne(user._id)
-        this.setState({ select: null, _id: '' })
-    }
+//     async handleDelete(user) {
+//         await db.collection('users').deleteOne(user._id)
+//         this.setState({ select: null, _id: '' })
+//     }
 
-    handleSelect(user) {
-        this.setState({ select: user, _id: user._id })
-    }
+//     handleSelect(user) {
+//         this.setState({ select: user, _id: user._id })
+//     }
 
-    async handleCreate() {
-        await db.collection('users').createOne({ _id: this.state._id, likes: [] })
-        this.setState({ _id: '' })
-    }
+//     async handleCreate() {
+//         await db.collection('users').createOne({ _id: this.state._id, likes: [] })
+//         this.setState({ _id: '' })
+//     }
 
-    async handleUpdate() {
-        await db.collection('users').deleteOne(this.state.select._id)
-        await db.collection('users').createOne({ _id: this.state._id })
-        this.setState({ select: null, _id: '' })
-    }
+//     async handleUpdate() {
+//         await db.collection('users').deleteOne(this.state.select._id)
+//         await db.collection('users').createOne({ _id: this.state._id })
+//         this.setState({ select: null, _id: '' })
+//     }
 
-    render() {
-        return (
-            <div>
-                <div style={{ padding: 10, backgroundColor: 'lightgreen' }}>
-                    <h2>My contacts List</h2>
-                    <List className='DataList'>
-                        <DataList collection={'users/' + db.user._id + '/contacts'} formatListItem={(user, i) => this.formatListUser(user, i)} />
-                    </List>
+//     render() {
+//         return (
+//             <div>
+//                 <div style={{ padding: 10, backgroundColor: 'lightgreen' }}>
+//                     <h2>My contacts List</h2>
+//                     <List className='DataList'>
+//                         <DataList collection={'users/' + db.user._id + '/contacts'} formatListItem={(user, i) => this.formatListUser(user, i)} />
+//                     </List>
 
-                    <p>Queries:</p>
-                    <TextField label='Username' value={this.state.search} onChange={e => this.setState({ search: e.target.value })} />
-                    <Button variant="raised" color="primary" size="small" style={{ float: 'right' }} onClick={() => this.handleSearchUsers()}>Search Users</Button>
-                    <p>Operations:</p>
-                    <TextField label='Username' value={this.state._id} onChange={e => this.setState({ _id: e.target.value })} />
-                    <Button size='small' variant="raised" color="primary" style={{ margin: 3, float: 'right' }} onClick={() => this.handleUpdate()}>Update</Button>
-                    <Button size='small' variant="raised" color="primary" style={{ margin: 3, float: 'right' }} onClick={() => this.handleCreate()}>Create</Button>
-                </div>
-                {
-                    this.state.select
-                        ?
-                        <div>
-                            <UserItemList user={this.state.select} />
-                            <LikedList user={this.state.select} />
-                        </div>
-                        :
-                        <div></div>
-                }
-            </div>
-        )
-    }
+//                     <p>Queries:</p>
+//                     <TextField label='Username' value={this.state.search} onChange={e => this.setState({ search: e.target.value })} />
+//                     <Button variant="raised" color="primary" size="small" style={{ float: 'right' }} onClick={() => this.handleSearchUsers()}>Search Users</Button>
+//                     <p>Operations:</p>
+//                     <TextField label='Username' value={this.state._id} onChange={e => this.setState({ _id: e.target.value })} />
+//                     <Button size='small' variant="raised" color="primary" style={{ margin: 3, float: 'right' }} onClick={() => this.handleUpdate()}>Update</Button>
+//                     <Button size='small' variant="raised" color="primary" style={{ margin: 3, float: 'right' }} onClick={() => this.handleCreate()}>Create</Button>
+//                 </div>
+//                 {
+//                     this.state.select
+//                         ?
+//                         <div>
+//                             <UserItemList user={this.state.select} />
+//                             <LikedList user={this.state.select} />
+//                         </div>
+//                         :
+//                         <div></div>
+//                 }
+//             </div>
+//         )
+//     }
 
-}
+// }
 
 
 export class details extends Component {
@@ -135,7 +135,16 @@ export class details extends Component {
     formatListItem(message, i) {
         return (
             <ListItem key={i}>
-                from: {message.from}, to: {message.to}, Message: {message.content}
+            {
+                message.to  == this.props.match.params._id || message.from == this.props.match.params._id
+                ?
+                <p>from: {message.from}, to: {message.to}, Message: {message.content}</p>
+                :
+                console.log()
+                
+            }
+
+                
             </ListItem>
         )
     }
@@ -157,7 +166,6 @@ export class details extends Component {
             &&
             <div style={{ padding: 10, backgroundColor: 'lightblue' }}>
                 <List className='DataList'>
-                    {/* <DataList collection={'items/' + this.state.item._id + '/messages'} formatListItem={(message, i) => this.formatListItem(message, i)} /> */}
                     <DataList collection={'users/' + db.user._id + '/messages'} formatListItem={(message, i) => this.formatListItem(message, i)} />
                 </List>
 
